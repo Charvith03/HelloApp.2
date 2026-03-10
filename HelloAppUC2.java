baf1c5e23c56df1e45ad7e542a8baf37230f3673
Charvith <charvith03@icloud.com>
2026-03-10 14:09:32 +0530

    1st

--- HelloAppUC2.java
+++ HelloAppUC2.java
@@ -1,9 +1,11 @@
+import java.util. Scanner;
 public class HelloAppUC2 {
 
     public static void main(String[] args) {
-
-        String name = (args.length > 0) ? args[0] : "Alice";
-        System.out.println("Hello " + name);
+        Scanner sc = new Scanner(System.in);
+        System.out .println("Enter your name");
+        String name = sc.next();
+        System.out.println("Hello " + name + "!");
 
     }
 
