141680e4015bc301cc1f7068b0ca7fe17d909855
Charvith <charvith03@icloud.com>
2026-03-11 22:29:08 +0530

    UC3

---
+++ HelloAppUC3.java
@@ -0,0 +1,13 @@
+public class HelloAppUC3 {
+   public HelloAppUC3() {
+   }
+
+   public static void main(String[] var0) {
+      String var1 = "World";
+      if (var0.length > 0) {
+         var1 = var0[0];
+      }
+
+      System.out.println("Hello, " + var1 + "!");
+   }
+}
